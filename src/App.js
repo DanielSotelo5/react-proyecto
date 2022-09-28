f5c68d4b414c385a7eab962d32265b2805a9d3ab
DanielSotelo <dany_99_rpmp@hotmail.com>
2022-09-28 19:26:46 -0300

    primer commmit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
+import CartProvider from './context/CartContext';
+import Cart from './components/Cart';
+import Form from './components/Form';
+import Contact from './components/Contact';
+import Success from './components/Success';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CartProvider>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route index element={<ItemListContainer greeting={"Catalogo"} />} />
+            <Route path="/:category" element={<ItemListContainer />} />
+            <Route path="/:category/item/:id" element={<ItemDetailContainer />} />
+            <Route path="/item/:id" element={<ItemDetailContainer />} />
+            <Route path="*" element={
+                <div style={{ backgroundColor: "red", width: '300px', height: '150px', margin: '0 auto' }}>ERROR 404 - NOT FOUND</div>
+              }
+            />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/form" element={<Form />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/success" element={<Success />} />
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </>
   );
 }
 
